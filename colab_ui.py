648c7914950d2b1d79f5de26499ea20913da6bc4
asvcode <25020209+asvcode@users.noreply.github.com>
2020-07-04 19:15:02 -0700

    Update colab_ui.py

--- colab_ui.py
+++ colab_ui.py
@@ -728,15 +728,13 @@ def lr_work():
     elif training.lr.value == '1e-1':
         lr_work.info = float(0.1)
 
+drive_upload()
+
 def colab_ui():
     from google.colab import widgets
     from google.colab import output
 
     t = widgets.TabBar(['Drive', 'Info', 'Data', 'Augmentation', 'Batch', 'Model', 'Metrics', 'Train'])
-
-    drive_upload()
-
-
     with t.output_to(0, select=False):
         #drive_upload()
         get_path()
